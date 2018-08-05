05181db7d7261e42b77d8d876ec4615c367bf2a2
narendra897715 <37704725+narendra897715@users.noreply.github.com>
2018-08-06 00:20:11 +0530

    Add files via upload

--- node-serverside/Server/app.js
+++ node-serverside/Server/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const http = require('http');
+const SocketIO = require('socket.io');
+const {generatedmessage} = require('./message/message')
 var app = express();
-app.listen(3000,()=>{
+var server = http.createServer(app);
+var io = SocketIO(server);
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+    socket.emit('newMessage',generatedmessage('Admin','welcome to chat room'));
+    socket.broadcast.emit('newMessage',generatedmessage('Admin','new user joined'))
+     socket.on('createMessage',(data,callback)=>{
+          io.emit('newMessage',generatedmessage(data.from,data.text),callback('got it data saved'));
+     })
+    socket.on('disconnect',(socket)=>{
+        console.log('disconnect from client');
+    })
+})
+server.listen(3000,()=>{
     console.log('port is listening to 3000')
 })
